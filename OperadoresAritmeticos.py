af6044dbc79921cd3c98b6f714b8c9a1ae982c50
dkzinn98 <derykalcinodasilva@icloud.com>
2025-12-02 12:36:27 -0300

    MoreLearning2

--- OperadoresAritmeticos.py
+++ OperadoresAritmeticos.py
@@ -1,10 +1,8 @@
 x = y = z = 0
-
 x = int(input('Digite um número: ')) # Convertendo o input para inteiros
 y = int(input('Digite outro número: ')) # Convertendo o input para inteiro
-
 z = x + y
-# print('A soma de x + y é:', z)
+print('A soma de x + y é:', z)
 
 # Desafio: melhore o código do exercício anterior, de modo que ele exiba além da soma: a subtração, a multiplicação,
 # a divisão, módulo (%), o primeiro elevao ao segundo. Entre os valores x e y digitados pelo usuário.
