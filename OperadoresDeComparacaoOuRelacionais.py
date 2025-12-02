6129438a0a0f9d118b7e585f82ff7698cd22d0b8
dkzinn98 <derykalcinodasilva@icloud.com>
2025-12-02 13:38:56 -0300

    MoreLearning

---
+++ OperadoresDeComparacaoOuRelacionais.py
@@ -0,0 +1,16 @@
+x = y = z = False
+n1 = n2 = 0
+
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+
+if n1 == n2:
+    print('São iguais!')
+elif n1 > n2:
+    print('n1 é maior que n2!')
+elif n1 < n2:
+    print('n1 é menor que n2!')
+else:
+    print('São diferentes!')
+
